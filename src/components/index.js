102a4183b568902ba59c7a4b0b9040d51935327e
jacobhogsten <hogsten1996@gmail.com>
2023-09-19 18:01:47 -0400

    ok

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export {default as Article} from './Article';
-export {default as Brand} from './Brand';
-export {default as CTA} from './CTA';
-export {default as Feature} from './Feature';
-export {default as Navbar} from './Navbar';
+export {default as Article} from './article/Article';
+export {default as Brand} from './brand/Brand';
+export {default as CTA} from './cta/CTA';
+export {default as Feature} from './feature/Feature';
+export {default as Navbar} from './navbar/Navbar';
